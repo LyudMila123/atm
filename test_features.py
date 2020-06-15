4d9f199c49c580b15e846145902a26bf490c6c22
Lyudmila <lyudmilachunihina@gmail.com>
2020-06-15 09:56:32 +0300

    test raise added

--- test_features.py
+++ test_features.py
@@ -37,6 +37,18 @@ class TestFeatures(unittest.TestCase):
     def test_enter_pin_correct_true(self):
         self.assertTrue(self.my_features.enter_pin(self.correct_pin))
 
+    # def test_enter_pin(self):
+    #     self.my_features.enter_pin(self.correct_pin)
+    #     self.attempts = self.attempts - 2
+    #     with self.assertRaises(AttemptsOver):
+    #         self.my_features.enter_pin(self.correct_pin)
+
+    def test_enter_pin_raises_incorrect_pin(self):
+        self.my_features.enter_pin(self.correct_pin)
+        self.attempts = self.attempts - 1
+        with self.assertRaises(IncorrectPin):
+            self.my_features.enter_pin(888)
+
     def test_enter_pin_correct_equal(self):
         my_pin_correct = self.my_features.enter_pin(self.correct_pin)
         self.assertEqual(my_pin_correct, True)
@@ -65,11 +77,26 @@ class TestFeatures(unittest.TestCase):
         self.atm_balance = self.atm_balance - 100
         self.assertEqual(self.my_features.get_money(9400), self.atm_balance)
 
+    def test_get_money_raise(self):
+        self.my_features.enter_pin(self.correct_pin)
+        self.atm_balance >= self.my_features.get_money(self.atm_balance)
+        with self.assertRaises(AtmBalance):
+            self.my_features.get_money(self.atm_balance)
+
     def test_check_balance(self):
         self.my_features.enter_pin(self.correct_pin)
         self.assertTrue(self.my_features.check_balance)
 
+    # def test_check_balance_raise(self):
+    #     self.my_features.enter_pin(self.correct_pin)
+    #     # self.atm_balance >= self.my_features.get_money(self.atm_balance)
+    #     with self.assertRaises(EnterPin):
+    #         self.my_features.get_money(self.atm_balance)
 
+
+
+
+     
 
 
 
